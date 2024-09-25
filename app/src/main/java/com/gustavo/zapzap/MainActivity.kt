0afe4ffb877988de1662f8a692ac78ba60d61d6e
gustavo.pardini <ggpardini@gmail.com>
2024-09-25 14:20:28 -0300

    Syncando a navegação entre telas com os highlits dos itens na bottombar

--- app/src/main/java/com/gustavo/zapzap/MainActivity.kt
+++ app/src/main/java/com/gustavo/zapzap/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.gustavo.zapzap
 
-import android.graphics.drawable.Icon
-import android.icu.text.CaseMap.Title
 import android.os.Build
 import android.os.Bundle
-import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -31,22 +28,19 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.gustavo.zapzap.ui.theme.Gray1DarkTheme
 import com.gustavo.zapzap.ui.theme.ZapZapTheme
 
 class MainActivity : ComponentActivity() {
@@ -104,6 +98,16 @@ fun App() {
     val pagerState = rememberPagerState {
         listaIconsBottomBar.size
     }
+
+    LaunchedEffect(selectedItem) {
+        pagerState.animateScrollToPage(listaIconsBottomBar.indexOf(selectedItem))
+    }
+
+    LaunchedEffect(pagerState.currentPage) {
+        selectedItem = listaIconsBottomBar[pagerState.currentPage]
+    }
+
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
